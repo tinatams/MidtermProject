fd3207582aaf40b8837278ce6eb298a83376e41b
VulterZ <126551220+VulterZ@users.noreply.github.com>
2025-02-19 20:35:55 +0800

    Add files via upload

--- SceneFrame.java
+++ SceneFrame.java
@@ -1,20 +1,35 @@
 import java.awt.*;
-import java.awt.geom.*;
 import javax.swing.*;
 
 public class SceneFrame{
     private JFrame frame= new JFrame();;
     private Container c=frame.getContentPane();
-    private DrawingCanvas scene1= new DrawingCanvas();
+    private P1 scene1= new P1();
+    private P2 scene2= new P2();
     
-    public void setUpGUI() {
+    public void setUpGUI() throws Exception{
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
         frame.setTitle("Midterm Project");
-        frame.add(scene1);
+        while(true){
+            frame.add(scene1);
+            frame.setVisible(true);
+            Thread.sleep(300);
+            frame.remove(scene1);
+            frame.revalidate();
+            frame.repaint();
+            
+            frame.add(scene2);
+            Thread.sleep(300);
+            frame.setVisible(true);
+            frame.remove(scene2);
+            frame.revalidate();
+            frame.repaint();
+            
+        }
 
         // Make the frame visible after all components are added
-        frame.setVisible(true);
+        
 
     }
 
