418a27762987d1c1a18f2a2f9d6b63376b64d172
Martina Llamas <177327336+tinatams@users.noreply.github.com>
2025-02-22 23:37:03 +0800

    Update SceneCanvas.java

--- SceneCanvas.java
+++ SceneCanvas.java
@@ -26,19 +26,22 @@ public class SceneCanvas extends JComponent {
 
         if(user.getX() < -300){
             ableAttack = true;
+            // need to find a way to make a timer for this so that when an action is committed it starts
+            // a timer of something and then after a certain amount of time the player can attack again.
         }
 
         int code = e.getKeyCode();
 
         if (code == KeyEvent.VK_Z && ableAttack){
             Kick.damage(npc);
+            ableAttack = false;
         } else if (code == KeyEvent.VK_X && ableAttack){
             Punch.damage(npc);
+            ableAttack = false;
         } else if (code == KeyEvent.VK_C && ableAttack){
             Fireball.damage(npc);
+            ableAttack = false;
         }
-        
-        System.out.print(npc.getHealth());
     }
 
 
