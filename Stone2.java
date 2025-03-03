3ff5f4e2cfe79e3184937d54b5a8dd462ab2bdd4
VulterZ <126551220+VulterZ@users.noreply.github.com>
2025-03-03 23:04:34 +0800

    Stone2.java

--- Stone2.java
+++ Stone2.java
@@ -21,10 +21,10 @@ import java.awt.*;
 import java.awt.geom.*;
 
 public class Stone2 implements DrawingObject{
-    private double x, y;
-    private Color beige= new Color(232, 191, 162);
-    private Color orange= new Color(200, 96, 78);
-    private Color wine_red= new Color(115, 39, 71);
+    public double x, y;
+    public Color beige= new Color(232, 191, 162);
+    public Color orange= new Color(200, 96, 78);
+    public Color wine_red= new Color(115, 39, 71);
 
     public Stone2(double x, double y) {
         this.x = x;
@@ -32,50 +32,36 @@ public class Stone2 implements DrawingObject{
     }
     @Override
     public void draw(Graphics2D g2d) {  
-        Rectangle2D.Double square = new Rectangle.Double(x+4.5*2, y+4.5*3, 4.5,4.5);
-        g2d.setColor(beige);
-        g2d.fill(square);
-        Rectangle2D.Double square2 = new Rectangle.Double(x+4.5, y+4.5*3, 4.5,4.5);
-        g2d.setColor(beige);
-        g2d.fill(square2);
-        Rectangle2D.Double square3 = new Rectangle.Double(x, y+4.5*3, 4.5,4.5);
-        g2d.setColor(orange);
-        g2d.fill(square3);
+        Square square = new Square(x+4.5*2, y+4.5*3, 4.5,beige);
+        Square square2 = new Square(x+4.5, y+4.5*3, 4.5,beige);
+        Square square3 = new Square(x, y+4.5*3, 4.5,orange);
 
-        Rectangle2D.Double square4 = new Rectangle.Double(x+4.5*2, y+4.5*2, 4.5,4.5);
-        g2d.setColor(beige);
-        g2d.fill(square4);
-        Rectangle2D.Double square5 = new Rectangle.Double(x+4.5, y+4.5*2, 4.5,4.5);
-        g2d.setColor(beige);
-        g2d.fill(square5);
-        Rectangle2D.Double square6 = new Rectangle.Double(x, y+4.5*2, 4.5,4.5);
-        g2d.setColor(orange);
-        g2d.fill(square6);
+        Square square4 = new Square(x+4.5*2, y+4.5*2, 4.5,beige);
+        Square square5 = new Square(x+4.5, y+4.5*2, 4.5,beige);
+        Square square6 = new Square(x, y+4.5*2, 4.5,orange);
 
+        Square square7 = new Square(x+4.5*3, y+4.5, 4.5,orange);
+        Square square8 = new Square(x+4.5*2, y+4.5, 4.5,orange);
+        Square square9 = new Square(x+4.5, y+4.5, 4.5,orange);
+        Square square10 = new Square(x, y+4.5, 4.5,wine_red);
 
-        Rectangle2D.Double square7 = new Rectangle.Double(x+4.5*3, y+4.5, 4.5,4.5);
-        g2d.setColor(orange);
-        g2d.fill(square7);
-        Rectangle2D.Double square8 = new Rectangle.Double(x+4.5*2, y+4.5, 4.5,4.5);
-        g2d.setColor(orange);
-        g2d.fill(square8);
-        Rectangle2D.Double square9 = new Rectangle.Double(x+4.5, y+4.5, 4.5,4.5);
-        g2d.setColor(orange);
-        g2d.fill(square9);
-        Rectangle2D.Double square10 = new Rectangle.Double(x, y+4.5, 4.5,4.5);
-        g2d.setColor(wine_red);
-        g2d.fill(square10);
+        Square square11= new Square(x+4.5*2, y, 4.5,orange);
+        Square square12= new Square(x+4.5, y, 4.5,orange);
+        Square square13= new Square(x, y, 4.5,wine_red);
 
-
-        Rectangle2D.Double square11= new Rectangle.Double(x+4.5*2, y, 4.5,4.5);
-        g2d.setColor(orange);
-        g2d.fill(square11);
-        Rectangle2D.Double square12= new Rectangle.Double(x+4.5, y, 4.5,4.5);
-        g2d.setColor(orange);
-        g2d.fill(square12);
-        Rectangle2D.Double square13= new Rectangle.Double(x, y, 4.5,4.5);
-        g2d.setColor(wine_red);
-        g2d.fill(square13);
+        square.draw(g2d);
+        square2.draw(g2d);
+        square3.draw(g2d);
+        square4.draw(g2d);
+        square5.draw(g2d);
+        square6.draw(g2d);
+        square7.draw(g2d);
+        square8.draw(g2d);
+        square9.draw(g2d);
+        square10.draw(g2d);
+        square11.draw(g2d);
+        square12.draw(g2d);
+        square13.draw(g2d);
     }
 
 }
