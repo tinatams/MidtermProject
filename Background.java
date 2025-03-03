6aaec609dec40510dd494d039252a4def0e8d61f
VulterZ <126551220+VulterZ@users.noreply.github.com>
2025-03-03 21:50:43 +0800

    Background.java

--- Background.java
+++ Background.java
@@ -18,50 +18,46 @@
 **/
 
 import java.awt.*;
-import java.awt.geom.AffineTransform;
+import java.awt.geom.*;
 
 public class Background{
     public void draw(Graphics2D g2d){
-        Crescent c=new Crescent(200,130);
-        Crescent c1=new Crescent(240,190);
-        Crescent c2=new Crescent(200,240);
-        Crescent c3=new Crescent(240,300);
-        Crescent c4=new Crescent(200,360);
-        Square column1_lava= new Square(-61.53,229.66,600,180);
-        Square h1_lava= new Square(54.15,394.97,45.98,6.11);
-        Square h2_lava= new Square(129.8,358.4,45.98,6.11);
-        Square h3_lava= new Square(137.44,428.25,45.98,6.11);
-        Square h4_lava= new Square(186.18,381.9,45.98,6.11);
-        Square h5_lava= new Square(229.35,404.14,45.98,4.08);
-        Square h6_lava= new Square(249.78,367.87,45.98,6.11);
-        Square h7_lava= new Square(307.86,390.05,45.98,4.08);
-        Square h8_lava= new Square(326.61,416.78,45.98,4.08);
-        Square h9_lava= new Square(366.65,376.97,45.98,4.08);
-        Square h10_lava= new Square(518.64,361.45,45.98,6.11);
-        Square h11_lava= new Square(394.9,411.09,45.98,4.08);
-        Square h12_lava= new Square(444.62,372.9,40.98,4.08 );
-        Square h13_lava= new Square(441.14,425.36,45.98,6.11);
-        Square h14_lava= new Square(470.38,388.01,40.98,4.08);
-        Square h15_lava= new Square(569.75,419.75,45.98,4.08);
-        Square h16_lava= new Square(508.09,431.05,45.98,6.11);
-        Square h17_lava= new Square(560.38,394.97,45.98,4.08);
-        Square h18_lava= new Square(610.36,363.8,40.98,4.08);
-        Square h19_lava= new Square(649.37,376.02,45.98,6.11);
-        Square h20_lava= new Square(647.28,427.23,40.98,4.08);
-        Square h21_lava= new Square(723.17,361.45,40.98,6.11);
-        Square h22_lava= new Square(695.34,398.03,33.14,4.08);
-        Square h23_lava= new Square(741.21,431.62,40.98,6.11);
-        Square h24_lava= new Square(394.9,350.09,45.98,4.08);
-        Square h25_lava= new Square(21.21,421.62,40.98,6.11);
-        Square h26_lava= new Square(16.21,370.62,40.98,6.11);
+        
+        Rectangle2D.Double column1_lava= new Rectangle2D.Double(-61.53,229.66,600,180);
 
-        Square lava= new Square(0,340.39,800,236.61);
-        Square platform= new Square(0,450.59,800,180);
-        Square up_platform= new Square(386.29,0,276.17,126.16);
-        Square up_platform2= new Square(658.21,0,81.79,91.16);
+        Rectangle2D.Double lava= new Rectangle2D.Double(0,340.39,800,236.61);
+        Rectangle2D.Double platform= new Rectangle2D.Double(0,450.59,800,180);
+        Rectangle2D.Double up_platform= new Rectangle2D.Double(386.29,0,276.17,126.16);
+        Rectangle2D.Double up_platform2= new Rectangle2D.Double(658.21,0,81.79,91.16);
         Triangle up_slant= new Triangle(662,86,41,41);
         Triangle up_slant2= new Triangle(739.46,-74.33,165.33,165.33);
         Triangle up_slant3= new Triangle(258.25,-37.37,128.79,128.79);
+        Rectangle2D.Double h1_lava= new Rectangle2D.Double(54.15,394.97,45.98,6.11);
+        Rectangle2D.Double h2_lava= new Rectangle2D.Double(129.8,358.4,45.98,6.11);
+        Rectangle2D.Double h3_lava= new Rectangle2D.Double(137.44,428.25,45.98,6.11);
+        Rectangle2D.Double h4_lava= new Rectangle2D.Double(186.18,381.9,45.98,6.11);
+        Rectangle2D.Double h5_lava= new Rectangle2D.Double(229.35,404.14,45.98,4.08);
+        Rectangle2D.Double h6_lava= new Rectangle2D.Double(249.78,367.87,45.98,6.11);
+        Rectangle2D.Double h7_lava= new Rectangle2D.Double(307.86,390.05,45.98,4.08);
+        Rectangle2D.Double h8_lava= new Rectangle2D.Double(326.61,416.78,45.98,4.08);
+        Rectangle2D.Double h9_lava= new Rectangle2D.Double(366.65,376.97,45.98,4.08);
+        Rectangle2D.Double h10_lava= new Rectangle2D.Double(518.64,361.45,45.98,6.11);
+        Rectangle2D.Double h11_lava= new Rectangle2D.Double(394.9,411.09,45.98,4.08);
+        Rectangle2D.Double h12_lava= new Rectangle2D.Double(444.62,372.9,40.98,4.08 );
+        Rectangle2D.Double h13_lava= new Rectangle2D.Double(441.14,425.36,45.98,6.11);
+        Rectangle2D.Double h14_lava= new Rectangle2D.Double(470.38,388.01,40.98,4.08);
+        Rectangle2D.Double h15_lava= new Rectangle2D.Double(569.75,419.75,45.98,4.08);
+        Rectangle2D.Double h16_lava= new Rectangle2D.Double(508.09,431.05,45.98,6.11);
+        Rectangle2D.Double h17_lava= new Rectangle2D.Double(560.38,394.97,45.98,4.08);
+        Rectangle2D.Double h18_lava= new Rectangle2D.Double(610.36,363.8,40.98,4.08);
+        Rectangle2D.Double h19_lava= new Rectangle2D.Double(649.37,376.02,45.98,6.11);
+        Rectangle2D.Double h20_lava= new Rectangle2D.Double(647.28,427.23,40.98,4.08);
+        Rectangle2D.Double h21_lava= new Rectangle2D.Double(723.17,361.45,40.98,6.11);
+        Rectangle2D.Double h22_lava= new Rectangle2D.Double(695.34,398.03,33.14,4.08);
+        Rectangle2D.Double h23_lava= new Rectangle2D.Double(741.21,431.62,40.98,6.11);
+        Rectangle2D.Double h24_lava= new Rectangle2D.Double(394.9,350.09,45.98,4.08);
+        Rectangle2D.Double h25_lava= new Rectangle2D.Double(21.21,421.62,40.98,6.11);
+        Rectangle2D.Double h26_lava= new Rectangle2D.Double(16.21,370.62,40.98,6.11);
 
         Stone1 stone=new Stone1(410.25,112.16);
         Stone1 stone1=new Stone1(390.25,112.16);
@@ -99,61 +95,30 @@ public class Background{
     g2d.translate(500, -25);
     originalTransform=g2d.getTransform();
     g2d.rotate(Math.toRadians(90),(-61.53+600/2),(229.66+180/2));
-    column1_lava.draw(g2d,new Color(200, 96, 78));
+    g2d.setColor(new Color(200, 96, 78));
+    g2d.fill(column1_lava);
     g2d.setTransform(originalTransform);
-    c.draw(g2d,new Color(252, 174, 25));
-    c1.draw(g2d,new Color(252, 174, 25));
-    c2.draw(g2d,new Color(252, 174, 25));
-    c3.draw(g2d,new Color(252, 174, 25));
-    c4.draw(g2d,new Color(252, 174, 25));
 
     g2d.translate(-500, 25);
 
     originalTransform=g2d.getTransform();
     
     g2d.rotate(Math.toRadians(90),(-61.53+600/2),(229.66+180/2));
-    column1_lava.draw(g2d,new Color(200, 96, 78));
+    g2d.setColor(new Color(200, 96, 78));
+    g2d.fill(column1_lava);
     g2d.setTransform(originalTransform);
 
-    c.draw(g2d,new Color(252, 174, 25));
-    c1.draw(g2d,new Color(252, 174, 25));
-    c2.draw(g2d,new Color(252, 174, 25));
-    c3.draw(g2d,new Color(252, 174, 25));
-    c4.draw(g2d,new Color(252, 174, 25));
     
-    lava.draw(g2d,new Color(200, 96, 78));
-    platform.draw(g2d,new Color(71, 19, 66));
-    h1_lava.draw(g2d,new Color(252, 174, 25));
-    h2_lava.draw(g2d,new Color(252, 174, 25));
-    h3_lava.draw(g2d,new Color(252, 174, 25));
-    h4_lava.draw(g2d,new Color(252, 174, 25));
-    h5_lava.draw(g2d,new Color(252, 174, 25));
-    h6_lava.draw(g2d,new Color(252, 174, 25));
-    h7_lava.draw(g2d,new Color(252, 174, 25));
-    h8_lava.draw(g2d,new Color(252, 174, 25));
-    h9_lava.draw(g2d,new Color(252, 174, 25));
-    h10_lava.draw(g2d,new Color(252, 174, 25));
-    h11_lava.draw(g2d,new Color(252, 174, 25));
-    h12_lava.draw(g2d,new Color(252, 174, 25));
-    h13_lava.draw(g2d,new Color(252, 174, 25));
-    h14_lava.draw(g2d,new Color(252, 174, 25));
-    h15_lava.draw(g2d,new Color(252, 174, 25));
-    h16_lava.draw(g2d,new Color(252, 174, 25));
-    h17_lava.draw(g2d,new Color(252, 174, 25));
-    h18_lava.draw(g2d,new Color(252, 174, 25));
-    h19_lava.draw(g2d,new Color(252, 174, 25));
-    h20_lava.draw(g2d,new Color(252, 174, 25));
-    h21_lava.draw(g2d,new Color(252, 174, 25));
-    h22_lava.draw(g2d,new Color(252, 174, 25));
-    h23_lava.draw(g2d,new Color(252, 174, 25));
-    h24_lava.draw(g2d,new Color(252, 174, 25));
-    h25_lava.draw(g2d,new Color(252, 174, 25));
-    h26_lava.draw(g2d,new Color(252, 174, 25));
+    g2d.setColor(new Color(200, 96, 78));
+    g2d.fill(lava);
+    g2d.setColor(new Color(71, 19, 66));
+    g2d.fill(platform);
 
     originalTransform = g2d.getTransform();
 
-    up_platform.draw(g2d,new Color(71, 19, 66));
-    up_platform2.draw(g2d,new Color(71, 19, 66));
+    g2d.setColor(new Color(71, 19, 66));
+    g2d.fill(up_platform);
+    g2d.fill(up_platform2);
 
     g2d.rotate(Math.toRadians(90),(662+41/2),(86+41/2));
     up_slant.draw(g2d,new Color(71, 19, 66));
@@ -210,8 +175,9 @@ public class Background{
 
     g2d.translate(-350, -50);
     originalTransform = g2d.getTransform();
-    up_platform.draw(g2d,new Color(71, 19, 66));
-    up_platform2.draw(g2d,new Color(71, 19, 66));
+    g2d.setColor(new Color(71, 19, 66));
+    g2d.fill(up_platform);
+    g2d.fill(up_platform2);
 
     g2d.rotate(Math.toRadians(90),(662+41/2),(86+41/2));
     up_slant.draw(g2d,new Color(71, 19, 66));
@@ -267,5 +233,35 @@ public class Background{
 
     g2d.translate(350, 50);
     
+    g2d.setColor(new Color(252, 174, 25));
+    g2d.fill(h1_lava);
+    g2d.fill(h2_lava);
+    g2d.fill(h3_lava);
+    g2d.fill(h4_lava);
+    g2d.fill(h5_lava);
+    g2d.fill(h6_lava);
+    g2d.fill(h7_lava);
+    g2d.fill(h8_lava);
+    g2d.fill(h9_lava);
+    g2d.fill(h10_lava);
+    g2d.fill(h11_lava);
+    g2d.fill(h12_lava);
+    g2d.fill(h13_lava);
+    g2d.fill(h14_lava);
+    g2d.fill(h15_lava);
+    g2d.fill(h16_lava);
+    g2d.fill(h17_lava);
+    g2d.fill(h18_lava);
+    g2d.fill(h19_lava);
+    g2d.fill(h20_lava);
+    g2d.fill(h21_lava);
+    g2d.fill(h22_lava);
+    g2d.fill(h23_lava);
+    g2d.fill(h24_lava);
+    g2d.fill(h25_lava);
+    g2d.fill(h26_lava);
+    
     }
 }
+
+ 
