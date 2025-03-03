130034e50fbc23508430de37602461c6b18a783f
VulterZ <126551220+VulterZ@users.noreply.github.com>
2025-03-03 23:03:20 +0800

    Square.java

--- Square.java
+++ Square.java
@@ -22,60 +22,25 @@ import java.awt.*;
 import java.awt.geom.*;
 
 public class Square implements DrawingObject{
-    private double x, y, w, h;
-    private Color color = Color.BLACK;
+    public double x, y, s;
+    public Color color;
 
-    /**
-     Constructor that initializes the values of the x and y position. As well as width and height of the shape.  
-
-     @param x = x position
-     @param y = y position
-     @param w = width
-     @param h = height
-     **/
-    public Square(double x, double y, double w, double h) {
+     public Square(double x, double y, double s, Color color) {
         this.x = x;
         this.y = y;
-        this.w = w;
-        this.h = h;
-    }
-
-    /**
-     Constructor that initializes the values of the x and y position, width and height, and color of the square.  
-
-     @param x = x position
-     @param y = y position
-     @param w = width
-     @param h = height
-     @param color = color
-     **/
-    public Square(double x, double y, double w, double h, Color color) {
-        this.x = x;
-        this.y = y;
-        this.w = w;
-        this.h = h;
+        this.s = s;
         this.color = color;
     }
 
-    /**
-    Draws the square object by creating an instance of Rectangle2D using the fields as the inputs. 
-    Sets the color of the Graphics2D object to color (field), and then fills in the shape.
-    of Rectangle2D.double object. 
-
-    @param is the Graphics2D object
-    **/
-
     @Override
     public void draw(Graphics2D g2d) {
-        Rectangle2D.Double square = new Rectangle.Double(x, y, w, h);
+        Rectangle2D.Double square = new Rectangle.Double(x, y, s,s);
         g2d.setColor(color);
         g2d.fill(square);
     }
 
-
-    //remove thisss
     public void draw(Graphics2D g2d, Color color) {
-        Rectangle2D.Double square = new Rectangle.Double(x, y, w, h);
+        Rectangle2D.Double square = new Rectangle.Double(x, y, s,s);
         g2d.setColor(color);
         g2d.fill(square);
     }
